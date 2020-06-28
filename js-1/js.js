397c5a3c8bd547634a0a04fd434e5cba1a6583ff
Mike Shibodaev <witcher759@gmail.com>
2020-06-29 02:16:23 +0400

    test callback

--- js-1/js.js
+++ js-1/js.js
@@ -1,4 +1,23 @@
-//function func1() {
-//    alert("Hello World");
-//};
-console.log( NaN == NaN);
+// function calbackTest(callback) {
+//     return callback;
+// }
+
+// function test() {
+//     alert("It's test");
+// }
+// calbackTest(test());
+
+
+
+
+/* Тест функции; */
+// function assert(value, desc) {
+//     var li = document.createElement("li");
+//     li.className = value ? "pass " : "fail";
+//     li.appendChild(document.createTextNode(desc));
+//     // document.getElementByid("results").appendChild(li);
+// };
+// window.onload = function() {
+//     assert(true, "The test suite i s running.");
+//     assert(false, " Fail 1 ");
+// };
