47fe79e93d1265c24b73fdd43a20f48bbeb6cf0f
Mike Shibodaev <witcher759@gmail.com>
2020-06-29 03:00:18 +0400

    test callback 1

--- js-1/js.js
+++ js-1/js.js
@@ -1,23 +1,8 @@
-// function calbackTest(callback) {
-//     return callback;
-// }
-
-// function test() {
-//     alert("It's test");
-// }
-// calbackTest(test());
+var friends = ["Mike", "Stacy", "Andy", "Rick"];
 
 
 
-
-/* Тест функции; */
-// function assert(value, desc) {
-//     var li = document.createElement("li");
-//     li.className = value ? "pass " : "fail";
-//     li.appendChild(document.createTextNode(desc));
-//     // document.getElementByid("results").appendChild(li);
-// };
-// window.onload = function() {
-//     assert(true, "The test suite i s running.");
-//     assert(false, " Fail 1 ");
-// };
+// http://javascriptissexy.com/understand-javascript-callback-functions-and-use-them/
+alert('ПОПРОБУЙ ДОПИСТАТЬ ФУНКЦИЮ КУРСА ВАЛЮТ');
+// var curse = JSON.parse('https://www.cbr-xml-daily.ru/daily_utf8.xml');
+// console.log(curse);
