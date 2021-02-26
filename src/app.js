59cc7c7562c2d99759eacf7e09ac00a530b96e0e
mshibodaev <mshibodaev@studiofact.ru>
2021-02-26 06:46:19 +0300

    работает cube

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
-// import myScripts from './js/index';
+import myScripts from './js/index';
 import './style/scss/main.scss';
 
+import Test from "./js/index";
 
-import * as THREE from 'three';
+let myTest = new Test(document.getElementById('canvas'));
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100);
-const renderer = new THREE.WebGLRenderer({antialias: true});
+myTest.init()
 
-renderer.setSize(window.innerWidth, window.innerHeight);
 
-document.body.appendChild(renderer.domElement)
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,43 @@
-// let myScripts = {
-//     run() {
-//         let i = 0;
-//         document.addEventListener('click' , function () {
-//             console.log(i++);
-//         })
-//     }
-// }
-// module.exports  =  myScripts;
+import * as THREE from 'three';
+
+function Test(selector) {
+    const self = this;
+    const size = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
+
+    const material = new THREE.MeshBasicMaterial({ 'color': 0x44aa88 });
+    let
+        camera = new THREE.PerspectiveCamera(45, size.width / size.height, 0.1, 100),
+        scene = new THREE.Scene(),
+        renderer, geometry = new THREE.BoxGeometry(1, 1, 1);
+    const cube = new THREE.Mesh(geometry, material);
+
+    renderer = new THREE.WebGLRenderer({
+        canvas: selector,
+        antialias: true
+    });
+
+    self.init = () => {
+        camera.position.set(1, 1, 5);
+        renderer.setSize(size.width, size.height);
+        scene.add(cube);
+        animate();
+    }
+
+    function  animate()  {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        camera.position.z += 0.01;
+
+
+    }
+
+}
+
+
+export default Test;
+
